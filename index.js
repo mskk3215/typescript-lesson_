6388cb6d5858901bc9ccd3f1d4661c0a07f5c4e0
mskk3215 <m.kaaiweb@gmail.com>
2023-07-08 23:43:50 +0900

    several type definition

--- index.js
+++ index.js
@@ -1,2 +1,66 @@
-var hello = "hello world";
-console.log(hello);
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = "hello";
+var double = "hello";
+var back = "hello";
+//object
+var person = { name: { first: "Jack", last: "Smith" }, age: 21 };
+//array
+var fruits = ["Apple", "Banana", "Grape"];
+//tuple
+var book = ["business", 1500, false];
+book.push(21);
+//enum 列挙型 =〜を書かなければ数値になる/ パスカルケース、全部大文字にする
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize[CoffeeSize["SHORT"] = 0] = "SHORT";
+    CoffeeSize[CoffeeSize["TALL"] = 1] = "TALL";
+    CoffeeSize[CoffeeSize["GRANDE"] = 2] = "GRANDE";
+    CoffeeSize[CoffeeSize["VENTI"] = 3] = "VENTI";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.TALL,
+};
+coffee.size = CoffeeSize.GRANDE;
+// console.log(coffee.size);
+//any型　 anyは何もしない型の恩恵は受けられない
+var anything = true;
+anything = "hello";
+anything = ["hello", 33, true];
+anything = {};
+anything.test = 111;
+var banana = "banana";
+banana = anything;
+// console.log(banana);
+//union型
+var unionType = 10;
+var unionTypes = [21, "hello"];
+console.log(unionTypes);
+//Literal型 constにすると自動的にLiteral型になる
+var apple = "apple";
+var clothSize = "large";
+//2つ3つとかであればenumではなくLiteral型を使った方がbetter
+var cloth = {
+    color: "white",
+    size: "medium",
+};
+var orange = "orange";
+var TshirtSize = "large"; //エイリアス
+//2つ3つとかであればenumではなくLiteral型を使った方がbetter
+var tshirt = {
+    color: "white",
+    size: "medium",
+};
+//関数への型定義
+function add(num1, num2) {
+    return num1 + num2;
+}
+// console.log(add(3, 2));
+//void型
+function sayHello() {
+    console.log("Hello!");
+}
+console.log(sayHello());
--- index.ts
+++ index.ts
@@ -1,8 +1,104 @@
-import axios from "axios";
+let hasValue: boolean = true;
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+let single: string = "hello";
+let double: string = "hello";
+let back: string = `hello`;
+//object
+const person = { name: { first: "Jack", last: "Smith" }, age: 21 };
+//array
+const fruits = ["Apple", "Banana", "Grape"];
 
-let hello: string = "hello world";
-console.log(hello);
+//tuple
+const book: [string, number, boolean] = ["business", 1500, false];
+book.push(21);
 
-function add(a: number, b: number): number {
-  return a + b;
+//enum 列挙型 =〜を書かなければ数値になる/ パスカルケース、全部大文字にする
+enum CoffeeSize {
+  SHORT,
+  TALL,
+  GRANDE,
+  VENTI,
 }
+const coffee = {
+  hot: true,
+  size: CoffeeSize.TALL,
+};
+coffee.size = CoffeeSize.GRANDE;
+// console.log(coffee.size);
+
+//any型　 anyは何もしない型の恩恵は受けられない
+let anything: any = true;
+anything = "hello";
+anything = ["hello", 33, true];
+anything = {};
+anything.test = 111;
+let banana = "banana";
+banana = anything;
+// console.log(banana);
+
+//union型
+let unionType: number | string = 10;
+let unionTypes: (number | string)[] = [21, "hello"];
+console.log(unionTypes);
+
+//Literal型 constにすると自動的にLiteral型になる
+const apple = "apple";
+let clothSize: "small" | "medium" | "large" = "large";
+//2つ3つとかであればenumではなくLiteral型を使った方がbetter
+const cloth: {
+  color: string;
+  size: "small" | "medium" | "large";
+} = {
+  color: "white",
+  size: "medium",
+};
+
+//typeエイリアス(別名)
+type TshirtSize = "small" | "medium" | "large"; //型を定義する
+const orange = "orange";
+
+let TshirtSize: TshirtSize = "large"; //エイリアス
+//2つ3つとかであればenumではなくLiteral型を使った方がbetter
+const tshirt: {
+  color: string;
+  size: TshirtSize; //エイリアス
+} = {
+  color: "white",
+  size: "medium",
+};
+
+//関数への型定義
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+// console.log(add(3, 2));
+
+//void型
+function sayHello(): void {
+  console.log("Hello!");
+}
+console.log(sayHello());
+
+//null型
+let tmp: null = null;
+
+//関数型 型の定義はどちらに合っても良い
+const anotherAdd: (n1: number, n2: number) => number = function add(
+  num1,
+  num2
+): number {
+  return num1 + num2;
+};
+// const doubleNumber = (number) => number * 2;
+const doubleNumber: (num: number) => number = (num) => num * 2;
+
+//callback関数に型をつける
+function doubleAndHandle(num: number, cb: (num: number) => number): void {
+  const doubleNum = cb(num * 2);
+  console.log(doubleNum);
+}
+doubleAndHandle(21, (doubleNum) => {
+  return doubleNum;
+});
